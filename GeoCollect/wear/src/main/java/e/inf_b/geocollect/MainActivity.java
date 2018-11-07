e8270ded798b37a7b3fa4d450baafe9601a17441
nachozamorano <36003700+nachozamorano@users.noreply.github.com>
2018-11-07 14:23:42 -0300

    Geocollect presentación

---
+++ GeoCollect/wear/src/main/java/e/inf_b/geocollect/MainActivity.java
@@ -0,0 +1,17 @@
+package e.inf_b.geocollect;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.wearable.intent.RemoteIntent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
